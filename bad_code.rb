cbc752af868f07811e796a83b5877e4158e0462a
Defoe.Brand <defoe.brand@gmail.com>
2020-08-13 17:14:48 +0300

    Fill in bad_code file

---
+++ bad_code.rb
@@ -0,0 +1,19 @@
+Class Bugs
+  def  spider 
+   end
+  
+  
+  def roly_poly
+  p "I'm a pill bug"  
+
+  end
+
+  def ant
+        p "I am mighty!"
+  end
+
+  def capetillar
+    p "I'm a beautiful butterfly!"
+  end
+  end
+end
