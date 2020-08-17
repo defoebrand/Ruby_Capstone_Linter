f70c9cf260b88d1dc47126c558b0426bb70c005a
Defoe.Brand <defoe.brand@gmail.com>
2020-08-17 16:35:04 +0300

    Create black/white version for repl.it website link

--- bin/ruby_linter.rb
+++ bin/ruby_linter.rb
@@ -1,11 +1,11 @@
 require 'colorize'
 require './lib/file_checker'
 
-puts 'Hello! This is a simple linter for Ruby'.bold
+puts 'Hello! This is a simple linter for Ruby'
 sleep(0.375)
-puts 'It will scan for whitespace errors, new line errors, indentation errors, and closing tag errors'.bold
+puts 'It will scan for whitespace errors, new line errors, indentation errors, and closing tag errors'
 sleep(0.375)
-puts 'Please enter the relative path of the file you would like to lint.'.bold
+puts 'Please enter the relative path of the file you would like to lint.'
 sleep(0.375)
 
 @filepath = gets.chomp
@@ -13,19 +13,19 @@ open_linter(@filepath)
 puts ''
 @errors = @error_hash.values.flatten
 if @errors.length.zero?
-  puts ' Congratulations! No errors were detected '.bold.blue.on_green
+  puts ' Congratulations! No errors were detected '
 else
   if @errors.length == 1
-    puts " A total of #{@errors.uniq.length} error has been found on the following line ".bold.red.on_light_white
+    puts " A total of #{@errors.uniq.length} error has been found on the following line "
   else
-    puts " A total of #{@errors.uniq.length} errors have been found on the following lines ".bold.red.on_light_white
+    puts " A total of #{@errors.uniq.length} errors have been found on the following lines "
   end
   sleep(0.5)
   @errors.uniq.length.times do |ind|
     puts ''
-    print "  Line  #{@errors.sort.uniq[ind]} has the following alert(s):".bold.red
+    print "  Line  #{@errors.sort.uniq[ind]} has the following alert(s):"
     @error_hash.each do |key, value|
-      print "    - #{key}".magenta if value.any?(@errors.sort.uniq[ind])
+      print "    - #{key}" if value.any?(@errors.sort.uniq[ind])
     end
     sleep(0.25)
   end
